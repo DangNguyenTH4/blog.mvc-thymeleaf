13108c54a8a32de1840a6aaa0e7979c459fa0951
da <dangnt520@wru.vn>
2020-12-01 02:46:35 +0700

    Fix css left menu, return not found page and status

--- src/main/java/dangnt/thymeleaf/controller/error/NotFoundController.java
+++ src/main/java/dangnt/thymeleaf/controller/error/NotFoundController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
@@ -13,6 +12,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class NotFoundController {
  @ExceptionHandler(value = NotFoundException.class)
   public ResponseEntity<String> notFound(NotFoundException no){
-   return new ResponseEntity<String>( "Not found", HttpStatus.NOT_FOUND);
+   return new ResponseEntity<String>( "<div style=\"width:100%; height:100%; background-color:red; background-image:url(https://i2.wp.com/learn.onemonth.com/wp-content/uploads/2017/08/1-10.png)\"><h1 style=\"text-align:center\">Not found</h1></div>", HttpStatus.NOT_FOUND);
  }
 }
